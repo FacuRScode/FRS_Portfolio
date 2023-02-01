d36980a66524f9f9a94905f6fcb6598f39f8a8f2
_FacuRS_ <88396491+facurs@users.noreply.github.com>
2023-02-01 16:22:36 -0300

    update app.js
    redactoring the function

--- public/js/app.js
+++ public/js/app.js
@@ -1,49 +1,36 @@
 /*
-const contactForm = document.querySelector('.contact-form');
-let formName = document.getElementById('formName');
-let email = document.getElementById('email');
-let subject = document.getElementById('subject');
-let message = document.getElementById('message');
-
-
-contactForm.addEventListener('submit', (e)=>{
-    e.preventDefault();
-    let formData = {
-        name: formName.value,
-        email: email.value,
-        subject: subject.value,
-        message: message.value
-    }
-    let xhr = new XMLHttpRequest();
-    xhr.open('POST','/');
-    xhr.setRequestHeader('content-type', 'application/json');
-    xhr.onload = function(){
-        if(xhr.responseText == 'success'){
-            alert('Email Sent');
-            formName.value = '';
-            email.value = '';
-            subject.value = '';
-            message.value = '';
-        }else{
-            alert('Something went wrong')
-        }
-    }
-
-    xhr.send(JSON.stringify(FormData));
-
-})
+Selects all elements with class "control" using document.querySelectorAll(".control") and converts it to an array using the spread operator (...).
+Loops through each button in the array using forEach() and adds a "click" event listener to it.
+When a button is clicked, the function removes the "active-btn" class from the current active button and adds it to the clicked button.
+The same happens for the corresponding elements with class "active". The related element is selected based on the data-id attribute of the clicked button.
+Additionally, it adds a "click" event listener to the element with class "theme-btn". When the theme button is clicked, the function toggles the "light-mode" class on the <body> element.
 */
 
-(function(){
-    [...document.querySelectorAll(".control")].forEach(button => {
-        button.addEventListener("click", function() {
-            document.querySelector(".active-btn").classList.remove("active-btn");
-            this.classList.add("active-btn");
-            document.querySelector(".active").classList.remove("active");
-            document.getElementById(button.dataset.id).classList.add("active");
-        })
+(function() {
+    const controlButtons = document.querySelectorAll(".control");
+    const themeButton = document.querySelector(".theme-btn");
+  
+    function handleControlButtonClick(event) {
+        const activeButton = document.querySelector(".active-btn");
+        activeButton.classList.remove("active-btn");
+        event.target.classList.add("active-btn");
+  
+        const activeElement = document.querySelector(".active");
+        activeElement.classList.remove("active");
+  
+        const targetId = event.target.dataset.id;
+        const targetElement = document.getElementById(targetId);
+        targetElement.classList.add("active");
+    }
+  
+    controlButtons.forEach(button => {
+        button.addEventListener("click", handleControlButtonClick);
     });
-    document.querySelector(".theme-btn").addEventListener("click", () => {
+  
+    function handleThemeButtonClick() {
         document.body.classList.toggle("light-mode");
-    })
+    }
+  
+    themeButton.addEventListener("click", handleThemeButtonClick);
 })();
+
